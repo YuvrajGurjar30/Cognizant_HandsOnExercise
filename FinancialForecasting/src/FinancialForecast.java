2fd267813d16c326e8f2175e376dab7b64daf42d
YuvrajGurjar30 <yuvrajsingh.tps@gmail.com>
2026-06-30 12:52:03 +0530

    Add Exercise 7: Financial Forecasting using Recursion

---
+++ FinancialForecasting/src/FinancialForecast.java
@@ -0,0 +1,13 @@
+public class FinancialForecast {
+
+    public static double calculateFutureValue(double currentValue, double growthRate, int years) {
+
+        // Base case
+        if (years == 0) {
+            return currentValue;
+        }
+
+        // Recursive case
+        return calculateFutureValue(currentValue, growthRate, years - 1) * (1 + growthRate);
+    }
+}
