982fd821114a8bb5a236f7694e98c6e799f22fc1
YuvrajGurjar30 <yuvrajsingh.tps@gmail.com>
2026-07-22 17:11:54 +0530

    .\Week 1 Java_Cognizant_Excercises"

---
+++ Week 1 Java_Cognizant_Excercises/FinancialForecasting/src/FinancialForecast.java
@@ -0,0 +1,13 @@
+public class FinancialForecast {
+
+    public static double calculateFutureValue(double currentValue, double growthRate, int years) {
+
+
+        if (years == 0) {
+            return currentValue;
+        }
+
+
+        return calculateFutureValue(currentValue, growthRate, years - 1) * (1 + growthRate);
+    }
+}
