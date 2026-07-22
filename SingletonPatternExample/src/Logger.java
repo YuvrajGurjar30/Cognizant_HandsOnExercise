29080edd6ca9c3102a8bd2c18e020edc7f6f8d5c
YuvrajGurjar30 <yuvrajsingh.tps@gmail.com>
2026-07-22 18:00:02 +0530

    Added Factory Method Pattern for document creation system

--- SingletonPatternExample/src/Logger.java
+++ SingletonPatternExample/src/Logger.java
@@ -1,23 +1,25 @@
 public class Logger {
+
+    // Step 1: Create a single object of Logger
     private static Logger instance;
 
-
+    // Step 2: Make constructor private
     private Logger() {
-        System.out.println("Logger Instance Created");
+        System.out.println("Logger Created");
     }
 
-
+    // Step 3: Public method to access object
     public static Logger getInstance() {
 
-        if (instance == null) {
+        if(instance == null) {
             instance = new Logger();
         }
 
         return instance;
     }
 
-
+    // Logging method
     public void log(String message) {
-        System.out.println("LOG: " + message);
+        System.out.println("LOG : " + message);
     }
 }
